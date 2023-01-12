9873bc6024286da22c280b987198e2ee7b3c9762
GarNepNep <taylor.nb@hotmail.com>
2023-01-12 22:48:45 +1300

    End of ep 8

--- assets/shaders/default.glsl
+++ assets/shaders/default.glsl
@@ -18,6 +18,9 @@ void main(){
 in vec4 fColor;
 out vec4 color;
 
-void main(){
-    color = fColor;
+uniform float uTime;
+
+void main() {
+    float avg = (fColor.r + fColor.g + fColor.b)/ 3;
+    color = sin(uTime) *fColor;
 }
--- src/main/java/jade/LevelEditorScene.java
+++ src/main/java/jade/LevelEditorScene.java
@@ -3,6 +3,7 @@ package jade;
 import org.joml.Vector2f;
 import org.lwjgl.BufferUtils;
 import renderer.Shader;
+import util.Time;
 
 import java.nio.FloatBuffer;
 import java.nio.IntBuffer;
@@ -105,7 +106,7 @@ public class LevelEditorScene extends Scene {
 		defaultShader.use();
 		defaultShader.uploadMat4f("uProjection", camera.getProjectionMatrix());
 		defaultShader.uploadMat4f("uView", camera.getViewMatrix());
-		
+		defaultShader.uploadFloat("uTime", Time.getTime());
 		
 		//bind the VAO
 		glBindVertexArray(vaoID);
